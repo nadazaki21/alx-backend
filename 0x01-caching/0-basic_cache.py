7e745b508cac07e7470598ccd60402791b9f99d0
nadazkai21 <nadazaki019@gmail.com>
2024-08-23 23:40:44 +0300

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" BasicCache module
+"""
+from parent_class import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+
+    def __init__(self):
+        """Initiliaze"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
